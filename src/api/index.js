953692c417a19c181ad5d1fb2eb595bb7b4a102f
Arthur Almeida <arthurdosantosalmeida@gmail.com>
2020-05-30 15:33:35 -0300

    Mock do conteúdo que será exibido no card do componente Post

---
+++ src/api/index.js
@@ -0,0 +1,64 @@
+const api = [
+  {
+    title: "First Title",
+    content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+    accumsan, justo vel tempus dignissim, tortor nisi porttitor velit,
+    nec sagittis purus dui sed magna. In mauris lorem, porttitor ac
+    ligula in, tincidunt varius purus. Ut id elit quis mauris
+    imperdiet ultricies eu id ligula. Nulla tristique mi sed metus
+    viverra hendrerit. Proin elementum diam quis erat ultricies
+    fermentum. Nulla et est quam. Etiam blandit purus sapien, vel
+    dictum dolor luctus vitae. Suspendisse eu massa dapibus mauris
+    placerat imperdiet.
+
+    Suspendisse nec diam felis. Donec pulvinar ipsum eu enim rhoncus,
+    sed fermentum lacus molestie. Quisque ut sapien metus. Maecenas in
+    sagittis tortor. Vestibulum rutrum tempus dignissim. Integer
+    convallis placerat tempor. Ut fermentum, nulla et congue
+    porttitor, diam dolor semper dolor, bibendum facilisis arcu ex sed
+    urna. Vivamus ex leo, pellentesque a justo vel, dapibus ultricies
+    eros. Duis sodales sapien eu nisl viverra luctus.`,
+  },
+  {
+    title: "Second Title",
+    content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+    accumsan, justo vel tempus dignissim, tortor nisi porttitor velit,
+    nec sagittis purus dui sed magna. In mauris lorem, porttitor ac
+    ligula in, tincidunt varius purus. Ut id elit quis mauris
+    imperdiet ultricies eu id ligula. Nulla tristique mi sed metus
+    viverra hendrerit. Proin elementum diam quis erat ultricies
+    fermentum. Nulla et est quam. Etiam blandit purus sapien, vel
+    dictum dolor luctus vitae. Suspendisse eu massa dapibus mauris
+    placerat imperdiet.
+
+    Suspendisse nec diam felis. Donec pulvinar ipsum eu enim rhoncus,
+    sed fermentum lacus molestie. Quisque ut sapien metus. Maecenas in
+    sagittis tortor. Vestibulum rutrum tempus dignissim. Integer
+    convallis placerat tempor. Ut fermentum, nulla et congue
+    porttitor, diam dolor semper dolor, bibendum facilisis arcu ex sed
+    urna. Vivamus ex leo, pellentesque a justo vel, dapibus ultricies
+    eros. Duis sodales sapien eu nisl viverra luctus.`,
+  },
+  {
+    title: "Third Title",
+    content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+    accumsan, justo vel tempus dignissim, tortor nisi porttitor velit,
+    nec sagittis purus dui sed magna. In mauris lorem, porttitor ac
+    ligula in, tincidunt varius purus. Ut id elit quis mauris
+    imperdiet ultricies eu id ligula. Nulla tristique mi sed metus
+    viverra hendrerit. Proin elementum diam quis erat ultricies
+    fermentum. Nulla et est quam. Etiam blandit purus sapien, vel
+    dictum dolor luctus vitae. Suspendisse eu massa dapibus mauris
+    placerat imperdiet.
+
+    Suspendisse nec diam felis. Donec pulvinar ipsum eu enim rhoncus,
+    sed fermentum lacus molestie. Quisque ut sapien metus. Maecenas in
+    sagittis tortor. Vestibulum rutrum tempus dignissim. Integer
+    convallis placerat tempor. Ut fermentum, nulla et congue
+    porttitor, diam dolor semper dolor, bibendum facilisis arcu ex sed
+    urna. Vivamus ex leo, pellentesque a justo vel, dapibus ultricies
+    eros. Duis sodales sapien eu nisl viverra luctus.`,
+  },
+];
+
+export default api;
